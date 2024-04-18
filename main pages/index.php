337838c87da41e39e9a0c310f8f7f7d2a0c892f6
YOUSSEF MOUSTAID <125811875+JosephMoustaid@users.noreply.github.com>
2024-04-18 16:19:50 +0100

    Add files via upload

---
+++ main pages/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ClubXtreme</title>
+    <!-- fontawesome icons -->  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- bootstrap icons-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body >
+
+<?php include("../components/nav.php") ?>
+<img class="home-page-img" src="../Images/Build Superhero-like Strength with Battle Ropes - Onnit Academy.jpg" alt="home page">
+
+<section class="container py-5">
+  <div class="row">
+    <h1>The Complete Club Solution</h1>
+    <div class="col-lg-4">
+      <h2>Website and App</h2>
+      <p>Create a smart website in minutes with free complimentary iOS and Android apps.</p>
+    </div>
+    <div class="col-lg-4">
+      <h2>Member Registration </h2>
+      <p>Register members with a custom form and store all membership records online.</p>
+    </div>
+    <div class="col-lg-4">
+      <h2>Online Payments</h2>
+      <p>Collect payments for membership and match fees using all major credit and debit cards.</p>
+    </div>
+    <div class="col-lg-4">
+      <h2>Team Management</h2>
+      <p>Give coaches digital tools to manage their fixtures, players and parents online.</p>
+    </div>
+    <div class="col-lg-4">
+      <h2>Competitions</h2>
+      <p>Update fixtures, results, match stats and league tables using the Pitchero mobile apps.</p>
+    </div>
+    <div class="col-lg-4">
+      <h2>GPS Trackers</h2>
+      <p>Improve player performance and reduce injuries with our innovative GPS player tracker.</p>
+    </div>
+  </div>
+</section>
+<section class="Testimony-section">
+
+</section>
+
+
+<script src="../node_modules/bootstrap/js/index.esm.js"></script>
+<script src="../node_modules/bootstrap/js/index.umd.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
